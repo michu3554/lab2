7b6a2192427d3c60491aaa5722243f539d4450df
michu3554 <variuxpl@gmail.com>
2021-03-08 08:09:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Zad 1.Napisz skrypt, w którym tworzysz listę z ulubionymi filmami,
+# a następnie odwróć całą listę. Po odwróceniu listy dodaj kilka pozycji
+#(dodane pozycje mają zostać dodane od 5 indeksu, cała lista mam zawierać
+#10 pozycji)
+
+# ulubione_filmy = ["Skazani na Shawshank","Nietykalni","Zielona mila","Ojciec Chrzestny","Forrest Gump","Pulp fiction"]
+# print(ulubione_filmy)
+# ulubione_filmy.reverse()
+# print(ulubione_filmy)
+# ulubione_filmy.insert(5,"Joker")
+# ulubione_filmy.insert(6,"Pianista")
+# ulubione_filmy.insert(7,"Incepcja")
+# ulubione_filmy.insert(8,"Milczenie owiec")
+# print(ulubione_filmy)
