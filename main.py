f71b94d7a8ba7c64c8bb3f402ef670dd4a2fcd96
michu3554 <variuxpl@gmail.com>
2021-03-08 08:20:44 +0100

    zad2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#Zad 1.Napisz skrypt, w którym tworzysz listę z ulubionymi filmami,
+# Zad 1.Napisz skrypt, w którym tworzysz listę z ulubionymi filmami,
 # a następnie odwróć całą listę. Po odwróceniu listy dodaj kilka pozycji
-#(dodane pozycje mają zostać dodane od 5 indeksu, cała lista mam zawierać
-#10 pozycji)
+# (dodane pozycje mają zostać dodane od 5 indeksu, cała lista mam zawierać
+# 10 pozycji)
 
 # ulubione_filmy = ["Skazani na Shawshank","Nietykalni","Zielona mila","Ojciec Chrzestny","Forrest Gump","Pulp fiction"]
 # print(ulubione_filmy)
@@ -12,3 +12,18 @@
 # ulubione_filmy.insert(7,"Incepcja")
 # ulubione_filmy.insert(8,"Milczenie owiec")
 # print(ulubione_filmy)
+# Zad 2. Stwórz skrypt, w którym utworzysz słownik z filmami z zadania 1. Pomyśl co może być kluczem
+
+slownik = {
+    "1": "Skazani na Shawshank",
+    "2": "Nietykalni",
+    "3": "Zielona mila",
+    "4": "Ojciec Chrzestny",
+    "5": "Forrest Gump",
+    "6": "Pulp fiction",
+    "7": "Joker",
+    "8": "Pianista",
+    "9": "Incepcja",
+    "10": "Milczenie owiec"
+}
+print(slownik)
