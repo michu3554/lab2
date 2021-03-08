49ea396fbaee9ae770f474df7b948a82da682314
michu3554 <variuxpl@gmail.com>
2021-03-08 08:25:02 +0100

    zad3

--- main.py
+++ main.py
@@ -12,18 +12,34 @@
 # ulubione_filmy.insert(7,"Incepcja")
 # ulubione_filmy.insert(8,"Milczenie owiec")
 # print(ulubione_filmy)
+
 # Zad 2. Stwórz skrypt, w którym utworzysz słownik z filmami z zadania 1. Pomyśl co może być kluczem
 
-slownik = {
-    "1": "Skazani na Shawshank",
-    "2": "Nietykalni",
-    "3": "Zielona mila",
-    "4": "Ojciec Chrzestny",
-    "5": "Forrest Gump",
-    "6": "Pulp fiction",
-    "7": "Joker",
-    "8": "Pianista",
-    "9": "Incepcja",
-    "10": "Milczenie owiec"
+# slownik = {
+#     "1": "Skazani na Shawshank",
+#     "2": "Nietykalni",
+#     "3": "Zielona mila",
+#     "4": "Ojciec Chrzestny",
+#     "5": "Forrest Gump",
+#     "6": "Pulp fiction",
+#     "7": "Joker",
+#     "8": "Pianista",
+#     "9": "Incepcja",
+#     "10": "Milczenie owiec"
+# }
+# print(slownik)
+
+# Zad 3. Stwórz skrypt, gdzie utworzysz słownik z nazwami przedmiotów z obecnego semestru oraz ich skrótami.
+# Policz liczbę elementów w słownik
+
+przedmioty = {
+    "CAD komputerowe wspomaganie programowania": "CAD",
+    "Wizualizacja danych": "WD",
+    "Przedmiot humanizujący": "PH",
+    "Język angielski": "JA",
+    "Rachunek różniczkowy i całkowy": "RRIC",
+    "Elementy matematyki dyskretnej": "EMD",
+    "Programowanie strukturalne": "PS"
 }
-print(slownik)
+print(przedmioty)
+print(len(przedmioty))
